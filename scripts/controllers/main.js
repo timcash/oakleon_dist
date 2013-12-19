25683aca35a496889b393eea15532277e7566c09
Tim <timcash@gmail.com>
2013-12-19 08:50:32 -0800

    demo

---
+++ scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('oakleonApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
